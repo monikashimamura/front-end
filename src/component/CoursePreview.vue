64b9bd46756e51e42290fdbae588baca03d69493
WorldOneTwo <2197248717>
2023-07-03 16:01:09 +0800

    更改

--- src/component/CoursePreview.vue
+++ src/component/CoursePreview.vue
@@ -17,19 +17,17 @@ export default {
 </script>
 
 <template>
-  <router-link to="/student/course">
-    <el-image class="imagePreview" :src="course.course.image" @click="lookCourse(course.course.cid)"></el-image>
-  </router-link>
   <div class="inline">
-    <el-text size="large" type="info"
-      >授课教师：</el-text
-    >
-    <el-text size="large" type="primary"
-      >{{ course.teacher }}</el-text
-    >
-  </div>
-  <div>
+    <router-link to="/student/course">
+      <el-image class="imagePreview" :src="course.course.image" @click="lookCourse(course.course.cid)"></el-image>
+    </router-link>
+    <div>
+      <el-text size="large" type="info">授课教师：</el-text>
+      <el-text size="large" type="primary">{{ course.teacher }}</el-text>
+    </div>
+    <div>
       <el-text size="small" type="info">{{ course.course.description }}</el-text>
+    </div>
   </div>
 </template>
 
