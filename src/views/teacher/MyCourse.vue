27adb61f0a68b2616c27b4692b76c7b00dc9343e
WorldOneTwo <2197248717>
2023-07-07 11:32:01 +0800

    我的课程列表

--- src/views/teacher/MyCourse.vue
+++ src/views/teacher/MyCourse.vue
@@ -6,6 +6,51 @@ import store from '@/store/store.js';
 import { UserFilled, Lock } from '@element-plus/icons-vue';
 import { useRouter } from 'vue-router';
 
+const bindData = reactive({
+	// 查询条件
+	myCourse: [],
+    totalNum: 0,
+    pageNum: 0,
+    pageSize: 3
+})
+
+onMounted(() => {
+	getMyCourse();
+});
+
+const getMyCourse = async () => {
+
+    await axios(store.url + '/course/getByTidPage', {
+		headers: {
+			Authorization: store.token
+		},
+        params: {
+            tid: store.user.uid,
+            page: bindData.pageNum,
+            size: bindData.pageSize
+        }
+	}).then(
+		res => {
+			if (res.data.code == 200) {
+				console.log(res.data);
+				bindData.myCourse = res.data.data.records
+				bindData.totalNum = res.data.data.total
+				console.log(bindData.itemList)
+				console.log(bindData.totalNum)
+			}
+			else {
+				alert(res.message)
+			}
+		}
+	)
+}
+
+const handlehCurrentChange = (val) => {
+	bindData.pageNum = Math.ceil(val)
+	console.log(bindData.pageNum)
+
+	getMyCourse()
+}
 
 
 </script>
@@ -19,7 +64,25 @@ import { useRouter } from 'vue-router';
     </div>
 
     <el-card class="main-card">
-    <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+        <el-card class="main-card">
+
+        <el-table :data="bindData.myCourse" border stripe size="large">
+            <el-table-column label="课程号" prop="cid"></el-table-column>
+            <el-table-column label="课程名" prop="name"></el-table-column>
+            <el-table-column label="简介" prop="description"></el-table-column>
+            <el-table-column label="审核状态" prop="status"></el-table-column>
+            <el-table-column label="操作">
+                <el-button type="primary" size="large" @click="onCancel">详情</el-button>
+                <el-button type="danger" size="large" @click="onSave">申请结课</el-button>
+
+            </el-table-column>
+        </el-table>
+
+		<div class="pagination">
+
+			<el-pagination @current-change="handlehCurrentChange" :page-size="bindData.pageSize" :current-page="bindData.pageNum" layout="prev, pager, next" :total="bindData.totalNum" size="large"/>
+		</div>
+	</el-card>
   </el-card>
 </template>
 
